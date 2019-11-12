397578c15c6f54977c7b4173cb192ffebf8f5772
Andrew E Slaughter <andrew.slaughter@inl.gov>
2019-11-12 03:42:25 -0600

    Clang-format

--- tutorials/darcy_thermo_mech/step12_equation_coupling/include/materials/PackedColumn.h
+++ tutorials/darcy_thermo_mech/step12_equation_coupling/include/materials/PackedColumn.h
@@ -28,79 +28,23 @@ public:
   PackedColumn(const InputParameters & parameters);
 
 protected:
-  /**
-   * Necessary override.  This is where the values of the properties
-   * are computed.
-   */
+  /// Necessary override. This is where the values of the properties are computed.
   virtual void computeQpProperties() override;
 
-  /**
-   * Helper function for reading CSV data for use in an interpolator object.
-   */
-  bool initInputData(const std::string & param_name, ADLinearInterpolation & interp);
-
   /// The radius of the spheres in the column
-  const Function & _input_radius;
+  const Function & _radius;
 
-  /// The input porosity
-  const Function & _input_porosity;
-
-  /// Temperature
-  const ADVariableValue & _temperature;
+  /// Value of viscosity from the input file
+  const Real & _input_viscosity;
 
   /// Compute permeability based on the radius (mm)
-  LinearInterpolation _permeability_interpolation;
-
-  /// Fluid viscosity
-  bool _use_fluid_mu_interp;
-  const Real & _fluid_mu;
-  ADLinearInterpolation _fluid_mu_interpolation;
-
-  /// Fluid thermal conductivity
-  bool _use_fluid_k_interp = false;
-  const Real & _fluid_k;
-  ADLinearInterpolation _fluid_k_interpolation;
-
-  /// Fluid density
-  bool _use_fluid_rho_interp = false;
-  const Real & _fluid_rho;
-  ADLinearInterpolation _fluid_rho_interpolation;
-
-  /// Fluid specific heat
-  bool _use_fluid_cp_interp;
-  const Real & _fluid_cp;
-  ADLinearInterpolation _fluid_cp_interpolation;
-
-  /// Solid thermal conductivity
-  bool _use_solid_k_interp = false;
-  const Real & _solid_k;
-  ADLinearInterpolation _solid_k_interpolation;
-
-  /// Solid density
-  bool _use_solid_rho_interp = false;
-  const Real & _solid_rho;
-  ADLinearInterpolation _solid_rho_interpolation;
-
-  /// Fluid specific heat
-  bool _use_solid_cp_interp;
-  const Real & _solid_cp;
-  ADLinearInterpolation _solid_cp_interpolation;
+  ADLinearInterpolation _permeability_interpolation;
 
   /// The permeability (K)
-  ADMaterialProperty<Real> & _permeability;
-
-  /// The porosity (eps)
-  ADMaterialProperty<Real> & _porosity;
+  ADMaterialProperty(Real) & _permeability;
 
   /// The viscosity of the fluid (mu)
-  ADMaterialProperty<Real> & _viscosity;
+  MaterialProperty<Real> & _viscosity;
 
-  /// The bulk thermal conductivity
-  ADMaterialProperty<Real> & _thermal_conductivity;
-
-  /// The bulk heat capacity
-  ADMaterialProperty<Real> & _specific_heat;
-
-  /// The bulk density
-  ADMaterialProperty<Real> & _density;
+  usingMaterialMembers;
 };
